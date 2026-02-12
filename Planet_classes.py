e426c6d28315c36185289db7caa3de8d92d8dda1
Ateh <atehnjitanji@gmail.com>
2026-02-12 17:59:58 -0500

    A to E

--- Planet_classes.py
+++ Planet_classes.py
@@ -1,9 +1,8 @@
-import pandas as pd 
+import pandas as pd
+
 df = pd.read_csv('planet_data.csv', index_col='eName')
 
 dff = df[['isPlanet','meanRadius','orbit_type','orbits']]
-
-print(dff.head())
 
 class planet(): 
     def __init__(self,name, color = "blue", radius = 1):
@@ -34,11 +33,11 @@ def print_largest(pl):
 
 planet_d = dict()
 moon_d = dict() 
-for index, row in df.iterrows(): 
+for index, row in dff.iterrows(): 
     if row['isPlanet'] is True:
         planet_d[index] = planet(name = index,radius= row['meanRadius'])
 
-for index, row in df.iterrows():
+for index, row in dff.iterrows():
     if row['isPlanet'] is False:
         moon_d[index] = moon(name = index,radius= row['meanRadius'], planet_companion= planet_d[row['orbits']], tidally_locked= row['orbit_type'])
 
@@ -52,3 +51,24 @@ for index, row in df.iterrows():
 '''for key, val in planet_d.items():
     print_largest(val)
     print(key, [moon.name for moon in val.moon_list])'''
+
+
+def order_planet(planet_ls):
+    #planet_data = pd.DataFrame(planet_ls.keys(), columns=['Planets'])
+    planet_data=planet_ls
+    #planet_data = planet_data.set_index('Planets')
+    for index, row in df.iterrows():
+        if row['isPlanet'] is True:
+            planet_data[index] = row['semimajorAxis_AU']
+            #planet_data.loc[index] = row['semimajorAxis_AU']
+    planet_data = pd.DataFrame({'Planet':planet_data.keys(),'Distance':planet_data.values()})
+    planet_data.sort_values(by='Distance',inplace=True)
+    '''i = 0
+    while i < planet_data.shape[0]:
+        if i > i+1:
+            df.loc[[i,i+1]] = df.loc[[i+1,i]].values
+            i = i + 1
+        else:
+            i = i + 1'''
+    print(planet_data['Planet'])
+order_planet(planet_d)
